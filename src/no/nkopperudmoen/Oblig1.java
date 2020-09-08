fb2de155f192ba9a61d9d9ad0c25ab82f4464afe
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2020-09-08 02:37:38 +0200

    Framskritt på oppgave 4

--- src/no/nkopperudmoen/Oblig1.java
+++ src/no/nkopperudmoen/Oblig1.java
@@ -1,15 +1,12 @@
 package no.nkopperudmoen;
 
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class Oblig1 {
-    /*
-      Nikolai Kopperudmoen, stud.nr s344169
-     */
+
     /**
+     * Nikolai Kopperudmoen, stud.nr s344169
      * Oppgave 1
      */
     public static int maks(int[] a) {
@@ -76,70 +73,51 @@ public class Oblig1 {
      * Oppgave 4
      */
     public static void delsortering(int[] a) {
-        sort(a, 0, a.length - 1);
-
-        /*if (a.length <= 1) {
-            return;
-        }
-        int value;
         int left = 0;
         int right = a.length - 1;
         for (int i = 0; i < a.length; i++) {
             while (a[right] % 2 == 0) {
                 right--;
             }
-            while (a[left] % 2 == 1) {
+            while (a[left] % 2 != 0) {
                 left++;
             }
             if (left < right) {
-                value = a[left];
+                int temp = a[left];
                 a[left] = a[right];
-                a[right] = value;
-            }
-        }*/
-    }
-
-    static void sort(int[] a, int left, int right) {
-        if (left < right) {
-            quicksortEO(a, left, right);
-            sort(a, left, right - 1);
-            sort(a, left + 1, right);
-        }
-    }
-
-    static int quicksort(int[] a, int start, int end) {
-        int pivot = a[end];
-        int i = start - 1;
-        for (int j = start; j < end; j++) {
-            if (a[j] <= pivot) {
-                i++;
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                a[right] = temp;
             }
         }
-        int temp = a[i + 1];
-        a[i + 1] = a[end];
-        a[end] = temp;
-        return i + 1;
-    }
-
-    static void quicksortEO(int[] a, int left, int right) {
-        while (left < right) {
-            int i = left - 1;
-            int pivot = a[right];
-            for (int l = left; l < right; l++) {
-                if (a[l] <= pivot) {
-                    i++;
-                    left++;
-                    int temp = a[i];
-                    a[i] = a[l];
-                    a[l] = temp;
-                }
+        left = 0;
+        right = a.length - 1;
+        int pivot = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                left = i;
+                pivot = i;
+                break;
             }
-            int temp = a[i + 1];
-            a[i + 1] = a[right];
-            a[right] = temp;
+        }
+        if (pivot == 0) {
+            return;
+        }
+        while (left <= right) {
+            if (a[left] > a[right]) {
+                int temp = a[right];
+                a[right] = a[left];
+                a[left] = temp;
+            }
+            right--;
+        }
+        right = pivot - 1;
+        left = 0;
+        while (left <= right) {
+            if (a[left] > a[right]) {
+                int temp = a[right];
+                a[right] = a[left];
+                a[left] = temp;
+            }
+            left++;
         }
     }
 
